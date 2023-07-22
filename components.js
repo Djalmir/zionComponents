fe80251cd8b3658638bb4c69a8ff09117d3662fe
Djalmir <djalmir_miodutzki@hotmail.com>
2023-07-22 00:41:51 -0300

    export test

--- components.js
+++ components.js
@@ -4,7 +4,13 @@ import zInput from './components/zInput.js'
 import zNumberInput from './components/zNumberInput.js'
 import zRadio from './components/zRadio.js'
 
-export {
+export const zCheckbox = zCheckbox
+export const zIcon = zIcon
+export const zInput = zInput
+export const zNumberInput = zNumberInput
+export const zRadio = zRadio
+
+export default {
 	zCheckbox,
 	zIcon,
 	zInput,
