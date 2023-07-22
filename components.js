c02efd61ae22f7ee14f3943994d916a932155d05
Djalmir <djalmir_miodutzki@hotmail.com>
2023-07-22 00:20:21 -0300

    exporting components

---
+++ components.js
@@ -0,0 +1,13 @@
+import zCheckbox from './components/zCheckbox.js'
+import zIcon from './components/zIcon.js'
+import zInput from './components/zInput.js'
+import zNumberInput from './components/zNumberInput.js'
+import zRadio from './components/zRadio.js'
+
+export {
+	zCheckbox,
+	zIcon,
+	zInput,
+	zNumberInput,
+	zRadio
+}
